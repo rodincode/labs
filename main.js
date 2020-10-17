e6f7a02ffde4a57dfd0adae1e8788c8f33739e35
rodincode <70000245+rodincode@users.noreply.github.com>
2020-10-17 16:43:02 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,46 @@
+anime({
+targets: 'div.box.red',
+translateY: [
+{value:200, duration: 1000 },
+{value:0, duration:800}
+],
+rotate:{
+value:'1turn',
+easing:'easeInOutSine'
+});
+
+anime({
+targets: 'div.box.blue',
+translateY: [
+{value:200, duration: 1000 },
+{value:0, duration:800}
+],
+rotate:{
+value:'1turn',
+easing:'easeInOutSine'
+});
+
+
+anime({
+targets: 'div.box.green',
+translateY: [
+{value:200, duration: 1000 },
+{value:0, duration:800}
+],
+rotate:{
+value:'1turn',
+easing:'easeInOutSine'
+});
+
+
+anime({
+targets: 'div.box.yellow',
+translateY: [
+{value:200, duration: 1000 },
+{value:0, duration:800}
+],
+rotate:{
+value:'1turn',
+easing:'easeInOutSine'
+});
+
